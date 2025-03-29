abfa7c9f824aed5643479953c5470dad1641effe
Matthew Warren <matthew.warren@wdidsbury.com>
2025-03-29 21:08:01 +0000

    Added Stoke data 

--- sample/data/postcode_parsing.py
+++ sample/data/postcode_parsing.py
@@ -3,7 +3,7 @@ import pandas as pd
 #OK, so I only really care about the postcode, and the coordintes of the given postcode. That is, the pcds (not 100% sure what the difference between pcd, pcds and pcd2 etc is, alas)
 #So I want to import the relevant areas, To my mind set does deliveries to stockport, leeds, manchester, chester, Liverpool etc. 
 # Looking at a postcode maps, we want to keep the following areas: 
-postcode_areas = ['SK', 'LS', 'M', 'CH', 'L', 'CW', 'WA', 'WN', 'BL', 'OL', 'HD', 'HX','WF', 'BB', 'BD', 'PR', 'LA' ]
+postcode_areas = ['SK', 'LS', 'M', 'CH', 'L', 'CW', 'WA', 'WN', 'BL', 'OL', 'HD', 'HX','WF', 'BB', 'BD', 'PR', 'LA','ST' ]
 
 #Now, we have large folder of postcode datas and want to pick the ones above out
 pathroot = 'sample/data/ONSPD_FEB_2025_UK_'
