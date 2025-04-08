32530a28726a686934a9b7d44495c8224f14e922
Matthew Warren <matthew.warren@wdidsbury.com>
2025-04-08 15:19:42 +0100

    Init file in tests

---
+++ tests/Imports.py
@@ -0,0 +1,4 @@
+from src.components import components as cp
+
+loci = cp.location('M202XE', [52,5])
+print(loci)
--- tests/testing.py
+++ tests/testing.py
@@ -6,18 +6,19 @@ import datetime as dt
 from context import src
 import os
 import sys
-from src import components as cp
+from src.components import components as cp
 
 
 #src/components/data/postcodes_coords.csv
 data = pd.read_csv('tests/postcodes_coords.csv')
-print(data.head())  
+
 
 #data has 384,872 row and 3 columns. Let's generate a random sample of 500 order locations!
 random_rows = np.random.randint(0, data.shape[0], 500)
+
 #For each order we also want generate an id and a cost. Here the id's will just be 'ord001' to 'ord100', and the cost will be a random number between 10 and 100.
 #N the number of orders. 
-N = 250
+N = 500
 
 order_ids = ['ord' + str(i).zfill(3) for i in range(501)]
 random_costs = np.random.randint(10,100,500)
@@ -32,3 +33,7 @@ Order_List = []
 
 for i in range(500):
     Order_List.append(cp.order(order_ids[i], random_locations[i], random_volumes[i], random_costs[i], 1, dt.datetime.now()))
+
+
+for x in Order_List[:5]:
+    print(x)
